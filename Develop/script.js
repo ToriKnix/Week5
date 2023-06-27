ce21a7016dfd98c13fa84fdbe22d0d9c7c9622ae
ToriKnix <toriknixmusic@gmail.com>
2023-06-26 18:20:28 -0600

    trying to get my repo to work

--- Develop/script.js
+++ Develop/script.js
@@ -1,55 +1,29 @@
 
-
-$(function() {
+$(function () {
   var currentHour = dayjs().hour();
-
-  var currentDay = dayjs().format("dddd, MMMM D, YYYY");
-  $("#currentDay").text(currentDay);
-  var startHour = 9; 
-  var endHour = 17; 
-  for (var hour = startHour; hour <= endHour; hour++) {
-    var timeBlockEl = $("<div>")
-      .attr("id", "hour-" + hour)
-      .addClass("row time-block");
-  var hourEl = $("<div>")
-      .addClass("col-2 col-md-1 hour text-center py-3")
-      .text(dayjs(hour, "H").format("ha"));
-  var descriptionEl = $("<textarea>")
-      .addClass("col-8 col-md-10 description")
-      .attr("rows", "3");
-
-  var saveBtnEl = $("<button>")
-      .addClass("btn saveBtn col-2 col-md-1")
-      .attr("aria-label", "save")
-      .html('<i class="fas fa-save" aria-hidden="true"></i>');
-
-    timeBlockEl.append(hourEl, descriptionEl, saveBtnEl);
-    $(".container-lg").append(timeBlockEl);
-  }
 
   $(".time-block").each(function() {
     var blockHour = parseInt($(this).attr("id").split("-")[1]);
-  if (blockHour < currentHour) {
+
+    if (blockHour < currentHour) {
       $(this).addClass("past");
-    } else if (blockHour === currentHour) {
+    }
+    else if (blockHour === currentHour) {
       $(this).addClass("present");
-    } else {
+    }
+    else {
       $(this).addClass("future");
     }
   });
 
-  $(".time-block").on("click", ".description", function() {
-    $(this).addClass("active");
-  });
-
-  $(".time-block").on("click", ".saveBtn", function() {
+  $(".saveBtn").on("click", function () {
     var timeBlock = $(this).parent();
     var description = timeBlock.find(".description").val();
     var hour = timeBlock.attr("id");
     localStorage.setItem(hour, description);
   });
-
-  $(".time-block").each(function() {
+  
+  $(".time-block").each(function () {
     var hour = $(this).attr("id");
     var description = localStorage.getItem(hour);
     if (description !== null) {
